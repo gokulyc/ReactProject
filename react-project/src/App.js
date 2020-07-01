3d5aba8868f68a31a9babc208740a31877e83bcd
Gokul <gokulyc@gmail.com>
2020-07-01 23:47:43 +0530

    47_50
     Passing Method References Between
    Components,Two way bind , swicthNameHandler, nameChangedHandler, style sheet and inline sheet

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -8,31 +8,55 @@ class App extends Component {
       { name: "Max", age: 26 },
       { name: "Gokul", age: 27 },
       { name: "Ravi", age: 29 }
-    ]
+    ],
+    other: "some oth value"
   }
 
-  swicthNameHandler = () => {
-    console.log("Button Clicked");
+  swicthNameHandler = (newName) => {
+    // console.log("Button Clicked");
     // this.state.persons[0].name = 'Maximilian'; not works
     this.setState({
       persons: [
-        { name: "Maximilian", age: 26 },
+        { name: "Max", age: 26 },
         { name: "Gokul", age: 27 },
         { name: "Ravi Kumar", age: 29 }
       ]
     });
   }
+
+  nameChangedHandler = (event) => {
+    this.setState(
+      {
+        persons: [
+          { name: "Max", age: 26 },
+          { name: event.target.value, age: 27 },
+          { name: "Ravi Kumar", age: 29 }
+        ]
+      }
+    )
+  }
   render() {
+    const stylebtn = {
+      backgroundColor: 'White',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+
+    };
     return (
       <div className="App" >
         <h1>Hi , Welcome to React app</h1>
-        <button onClick={this.swicthNameHandler}>Switch Name</button>
+        {/* bind argument with function 1st way || efficient way <button onClick={this.swicthNameHandler.bind(this, 'Maximilian')}>Switch Name</button> */}
+        <button style={stylebtn}
+          onClick={() => this.swicthNameHandler('Maximilian!')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Hobbies: Netflix</Person>
+        <Person changed={this.nameChangedHandler} click={this.swicthNameHandler.bind(this, 'Max!!!')} name={this.state.persons[1].name} age={this.state.persons[1].age} >Hobbies: Netflix</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
+      </div >
     );
   }
 }
+
 
 export default App;
--- react-project/src/Person/Person.js
+++ react-project/src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css'
 const person = (props) => {
     return (
-        <div>
-            <span>I'm {props.name} and age is {props.age}  </span>
+        <div className="Person">
+            <span onClick={props.click}>I'm {props.name} and age is {props.age}  </span>
             <span>{props.children}</span>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
